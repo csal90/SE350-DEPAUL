1cd3d0de028185c4a7a2b371cd569aabad0c0a64
csal90 <35342933+csal90@users.noreply.github.com>
2021-01-18 18:58:39 -0600

    added assignment 3 files

---
+++ src/hw3/fpTest.java
@@ -0,0 +1,99 @@
+package hw3;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class fpTest {
+
+	@Test
+	void testMap() {
+		LinkedList<Integer> l = new LinkedList<>();
+		for (int i = 0; i < 5; i++)
+			l.addFirst(i + 1);
+		List<Boolean> b = fp.map(l, (Integer x) -> {
+			return x % 2 == 0;
+		});
+		Boolean[] s = { false, true, false, true, false };
+		assertArrayEquals(b.toArray(), s);
+
+		List<Integer> u = fp.map(l, (Integer x) -> {
+			return x + 1;
+		});
+		Integer[] r = { 6, 5, 4, 3, 2 };
+		assertArrayEquals(u.toArray(), r);
+	}
+
+	@Test
+	void testFoldLeft() {
+
+		LinkedList<Integer> l = new LinkedList<>();
+		assertTrue(0 == fp.foldLeft(0, l, (Integer x, Integer y) -> {
+			return x + y;
+		}));
+		for (int i = 0; i < 10; i++)
+			l.addLast(i + 1);
+		assertTrue(55 == fp.foldLeft(0, l, (Integer x, Integer y) -> {
+			return x + y;
+		}));
+
+		LinkedList<String> s = new LinkedList<>();
+		String r = "";
+		for (int i = 0; i < 4; i++) {
+			s.addLast(new Integer(i).toString());
+			r = r.concat(new Integer(i).toString());
+		}
+		assertTrue(r.equals(fp.foldLeft("", s, (String x, String y) -> {
+			return x.concat(y);
+		})));
+	}
+
+	@Test
+	void testFoldRight() {
+
+		LinkedList<String> s = new LinkedList<>();
+		String r = "";
+		for (int i = 0; i < 4; i++) {
+			s.addLast(new Integer(i).toString());
+			r = new Integer(i).toString().concat(r);
+		}
+		assertTrue(r.equals(fp.foldRight("", s, (String x, String y) -> {
+			return x.concat(y);
+		})));
+
+		LinkedList<Integer> l1 = new LinkedList<>();
+		for (int j = 0; j < 3; j++)
+			l1.addLast(j + 1);
+		// [1,2,3]
+		assertTrue(2 == fp.foldRight(0, l1, (Integer x, Integer y) -> {
+			return x - y;
+		}));
+		// 3 -0 =3. 2-3 =-1. 1-(-1) = 2
+	}
+
+	@Test
+	void testFilter() {
+		LinkedList<Integer> l = new LinkedList<>();
+		for (int i = 0; i < 5; i++)
+			l.addFirst(i + 1);
+		Iterable<Integer> i = fp.filter(l, (Integer x) -> {
+			return x % 2 != 0;
+		});
+		int u = 0;
+		for (Integer a : i)
+			u++;
+		assertTrue(u == 3);
+
+		Iterable<Integer> j = fp.filter(l, (Integer x) -> {
+			return x % 2 == 0;
+		});
+		int u1 = 0;
+		for (Integer a : j) {
+			u1++;
+		}
+		assertTrue(u1 == 2);
+	}
+
+}
